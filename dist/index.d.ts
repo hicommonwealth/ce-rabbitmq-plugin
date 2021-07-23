a569405cb9e729e04b4162216b3e2f5e6a773173
timolegros <timothee@ualberta.ca>
2021-07-23 23:09:04 +0200

    build files

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,4 @@
 import RabbitMqDefaultConfig from './RabbitMQconfig.json';
 import RabbitMqIdentityConfig from './WithIdentityQueueConfig.json';
-
 export { RabbitMqDefaultConfig, RabbitMqIdentityConfig };
 export * from './src';
-
