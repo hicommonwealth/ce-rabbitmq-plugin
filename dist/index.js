2ce9ad8be515f5717b3fe4c6f369e79ba9246915
timolegros <timothee@ualberta.ca>
2021-07-23 23:29:09 +0200

    build files

--- dist/index.js
+++ dist/index.js
@@ -6,18 +6,6 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
@@ -26,8 +14,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RabbitMqIdentityConfig = exports.RabbitMqDefaultConfig = exports.RabbitMqHandler = exports.RabbitMqProducer = void 0;
-exports.RabbitMqProducer = __importStar(require("./producer"));
-exports.RabbitMqHandler = __importStar(require("./handler"));
+var producer_1 = require("./producer");
+Object.defineProperty(exports, "RabbitMqProducer", { enumerable: true, get: function () { return producer_1.RabbitMqProducer; } });
+var handler_1 = require("./handler");
+Object.defineProperty(exports, "RabbitMqHandler", { enumerable: true, get: function () { return handler_1.RabbitMqHandler; } });
 __exportStar(require("./util"), exports);
 const RabbitMQconfig_json_1 = __importDefault(require("./RabbitMQconfig.json"));
 exports.RabbitMqDefaultConfig = RabbitMQconfig_json_1.default;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export * as RabbitMqProducer from "./producer"
-export * as RabbitMqHandler from "./handler"
+export { RabbitMqProducer }  from "./producer"
+export { RabbitMqHandler } from "./handler"
 export * from "./util"
 
 import RabbitMqDefaultConfig from './RabbitMQconfig.json';
